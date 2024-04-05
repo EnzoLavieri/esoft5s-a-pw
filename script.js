1559d2f809bca9b07beecc6e982f863858d539cb
Enzo D Andrey Lavieri Yarid <128073322+EnzoLavieri@users.noreply.github.com>
2024-04-05 01:21:15 +0000

    atv 04-04-24

---
+++ script.js
@@ -0,0 +1,7 @@
+function fetchApiData(){
+    fetch("https://pokeapi.co/api/v2/pokemon/")
+        .then(Response => Response.json())
+        .then(data => {
+            const list = document.querySelector
+        })
+}   

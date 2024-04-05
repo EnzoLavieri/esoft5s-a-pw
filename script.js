229e9ba92743cab2f337cec747b579f348c3aaab
Enzo D Andrey Lavieri Yarid <128073322+EnzoLavieri@users.noreply.github.com>
2024-04-05 04:11:21 +0000

    atualização script de api, styles e index.html

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-function fetchApiData(){
-    fetch("https://pokeapi.co/api/v2/pokemon/")
-        .then(Response => Response.json())
-        .then(data => {
-            const list = document.querySelector
-        })
-}   
+
+async function main() {
+    let evolucaoPokemon = new URLSearchParams(document.location.search).get("evolucao");
+    document.querySelector("#title").textContent = "Página do " + evolucaoPokemon;
+
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${evolucaoPokemon}`);
+    const imageUrl = await res.json().then((response) => { 
+        return response.sprites.front_default 
+    });
+
+    const image = document.createElement("img");
+    image.src = imageUrl;
+    image.alt = "Imagem do " + evolucaoPokemon;
+
+    const h3 = document.createElement("h3");
+    h3.textContent = "Informações sobre o " + evolucaoPokemon;
+
+    document.querySelector("#pokemon-card").appendChild(h3);
+    document.querySelector("#pokemon-card").appendChild(image);
+    
+}
+
+main();
